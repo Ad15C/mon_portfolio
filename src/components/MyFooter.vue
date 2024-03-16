70a3a345b92cd1d3374796b6b9cbd3faaf84406c
Adeline <ad15canon@gmail.com>
2024-03-16 11:02:15 +0100

    update_3

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,20 +1,15 @@
 <template>
- <footer>
-   <div class="Networks">
-            <a href="https://fr.linkedin.com/">
-                <img src="@/assets/linkedin.png" alt="Profil_LinkedIn">
-            </a>
-                
-            <a href="https://twitter.com/?lang=fr">
-                <img src="@/Images/twitter.png" alt="Profil_Twitter">
-            </a>
+  <footer>
+    <div class="Networks">
+      <a href="https://fr.linkedin.com/">
+        <img src="@/assets/linkedin.png" alt="Profil_LinkedIn" />
+      </a>
+      <a href="https://github.com/Ad15C">
+        <img src="@/assets/signe-github.png" alt="Profil_GitHub" />
+      </a>
+    </div>
 
-            <a href="https://github.com/Ad15C">
-                <img src="./Images/signe-github.png" alt="Profil_GitHub">
-            </a>
-        </div>
-    
-        <h6>Dernière mise à jour le 10 Août 2022</h6>
+    <h6>Dernière mise à jour le 10 Août 2022</h6>
   </footer>
 </template>
 
