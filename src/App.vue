e9805bce1db9f395126f76e8601c04761806e82d
Adeline <ad15canon@gmail.com>
2024-03-16 13:01:52 +0100

    assets

--- src/App.vue
+++ src/App.vue
@@ -14,23 +14,33 @@
 import MyHeader from '@/components/MyHeader.vue'
 import MyPage from '@/components/MyPage.vue'
 import MyFooter from '@/components/MyFooter.vue'
-import { reactive } from 'vue';
+import { reactive } from 'vue'
 
-const aboutMe = 'Ma mission consiste à aider mes clients à réaliser leurs projets';
+const aboutMe = 'Ma mission consiste à aider mes clients à réaliser leurs projets'
 
-const user = reactive ({
-  yourName:'Veuillez indiquer votre Nom',
-  yourMail:'Veuillez indiquer votre Mail',
-  yourMessage: 'Veuillez saisir votre message',
-});
+const user = reactive({
+  yourName: 'Veuillez indiquer votre Nom',
+  yourMail: 'Veuillez indiquer votre Mail',
+  yourMessage: 'Veuillez saisir votre message'
+})
 
-const display = 0 => {
-  console.log (user.yourName, user.yourMail, user.yourMessage);
+const display = 0
+{
+  console.log(user.yourName, user.yourMail, user.yourMessage)
 }
 </script>
 
 <style scoped>
 main {
+  background-color: #e6e6e6;
+}
+
+section {
+  display: flex;
+  background-color: blue;
+}
+
+footer {
   background-color: #e6e6e6;
   display: flex;
   flex-direction: column;
--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -2,10 +2,10 @@
   <footer>
     <div class="Networks">
       <a href="https://fr.linkedin.com/">
-        <img src="@/assets/linkedin.png" alt="Profil_LinkedIn" />
+        <img src="@/assets/Linkedin.png" alt="Profil_LinkedIn" width="15" height="15" />
       </a>
       <a href="https://github.com/Ad15C">
-        <img src="@/assets/signe-github.png" alt="Profil_GitHub" />
+        <img src="@/assets/github.png" alt="Profil_GitHub" width="15" height="15" />
       </a>
     </div>
 
