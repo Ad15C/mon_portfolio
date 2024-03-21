1e8347eb44f1e94a8ba3787f8c86469e27c7bdec
Adeline <ad15canon@gmail.com>
2024-03-21 19:03:30 +0100

    maj

--- src/components/MyForm.vue
+++ src/components/MyForm.vue
@@ -1,34 +1,33 @@
 <template>
-  <form @submit.prevent="validateForm" id="myForm" method="post">
+  <form @submit.prevent="validateInfo" id="myForm" method="post">
     <p>
-      Pour toutes informations, remplissez le formulaire et je reviendrai vers vous dés que
-      possisble.
+      Pour toutes informations, remplissez le formulaire ci-dessous. Je reviendrai vers vous dés que
+      possible.
     </p>
     <br />
 
     <label for="lastName">Votre Nom:</label><br />
     <input type="text" id="lastName" v-model.trim="user.lastName" /><br />
 
-    <label for="firstName" id="firstName">Votre Prénom:</label><br />
+    <label for="firstName">Votre Prénom:</label><br />
     <input type="text" id="firstName" v-model.trim="user.firstName" /><br />
 
-    <label for="yourMail" id="yourMail">Votre Email:</label><br />
-    <input type="email" id="yourMail" v-model.trim="user.yourMail" /><br />
+    <label for="yourMail">Votre Email:</label><br />
+    <input type="email" id="yourMail" v-model.trim="user.yourMail" /> <br />
 
-    <label for="yourMessages" id="yourMessages">Votre Message:</label><br />
-    <textarea v-model.trim="user.yourMessages"> </textarea><br />
+    <label for="yourMessages">Votre Message:</label><br />
+    <input type="textarea" id="yourMessages" v-model.trim="user.yourMessages" /><br />
 
     <!-- error message -->
     <div v-if="errorMessage">{{ errorMessage }}</div>
-
     <button type="submit">Envoyer</button>
   </form>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { errorMessages } from 'vue/compiler-sfc'
 
-//Champs du formulaire
 const user = ref({
   lastName: '',
   firstName: '',
@@ -36,17 +35,15 @@ const user = ref({
   yourMessages: ''
 })
 
-const errorMessage = 'Veuillez remplir les champs,merci.'
-
 const display = () => {
   console.log(user)
 }
-
-function validateForm(e) {
-  //on valide les champs
-  if (this.lastName || this.firstName || this.yourMail || this.yourMessages) {
-    errorMessage
+function validateInfo(e) {
+  if (!this.lastName || !this.firstName || !this.yourMail || !this.yourMessages) {
+    errorMessages
   }
+
+  e.preventDefault()
 }
 </script>
 
