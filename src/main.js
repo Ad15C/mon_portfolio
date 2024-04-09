db9b8a640328987d9d8c08213d699b74d0f577da
Adeline <ad15canon@gmail.com>
2024-04-09 17:09:01 +0200

    router_6

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from './views/Homepage.vue'
 
+const routes = [{ path: '/', name: 'Accueil', component: Homepage }]
+
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/', name: 'Accueil', component: Homepage }]
+  routes
 })
 
 const app = createApp(App)
