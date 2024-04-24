ce3c876ade84046ce8bb475b111b42a7508ac058
Adeline <ad15canon@gmail.com>
2024-04-24 17:29:01 +0200

    modal_6

---
+++ src/components/Modal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="modal-mask">
+    <div class="modal-container">
+      <header>header text</header>
+      <div>main text</div>
+      <footer>footer</footer>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
